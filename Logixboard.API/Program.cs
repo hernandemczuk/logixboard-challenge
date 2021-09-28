3ecd6e7a2b501f6823cef7d9ec21ab9e5866c56a
Hernan Demczuk <hernandemczuk@gmail.com>
2021-09-27 22:01:28 -0300

    Include missing references

--- Logixboard.API/Program.cs
+++ Logixboard.API/Program.cs
@@ -1,11 +1,19 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
+using Logixboard.Database;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json.Linq;
+using System.IO;
+using Logixboard.Models;
+using Logixboard.API.DTOs;
 
 namespace Logixboard.API
 {
