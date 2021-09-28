112efb31eed556e1ed4ad1b8b03b746fe54570ad
Hernan Demczuk <hernandemczuk@gmail.com>
2021-09-27 21:58:03 -0300

    Add database project

---
+++ Logixboard.Database/ApplicationDbContext.cs
@@ -0,0 +1,40 @@
+using System.IO;
+using Logixboard.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace Logixboard.Database
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public virtual DbSet<Shipment> Shipments { get; set; }
+        public virtual DbSet<Organization> Organizations { get; set; }
+        public virtual DbSet<Node> Nodes { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Shipment>().HasKey(c => c.ReferenceId);
+            modelBuilder.Entity<Shipment>().HasMany(c => c.TransportPacks).WithOne(x => x.Shipment).OnDelete(DeleteBehavior.Cascade);
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+
+    public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext> 
+    {
+        public ApplicationDbContext CreateDbContext(string[] args) 
+        { 
+            IConfigurationRoot configuration = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile(@Directory.GetCurrentDirectory() + "/../Logixboard.API/appsettings.json").Build();
+            var builder = new DbContextOptionsBuilder<ApplicationDbContext>();
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+            builder.UseSqlServer(connectionString);
+            return new ApplicationDbContext(builder.Options);
+        }
+    }
+}
---
+++ Logixboard.Database/Migrations/20210928005714_Add Initial Migration.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Logixboard.Database.Migrations
+{
+    public partial class AddInitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Organizations",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Code = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Organizations", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Shipments",
+                columns: table => new
+                {
+                    ReferenceId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    EstimatedTimeArrival = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Shipments", x => x.ReferenceId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Nodes",
+                columns: table => new
+                {
+                    NodeId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Weight = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Unit = table.Column<int>(type: "int", nullable: false),
+                    ShipmentReferenceId = table.Column<string>(type: "nvarchar(450)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Nodes", x => x.NodeId);
+                    table.ForeignKey(
+                        name: "FK_Nodes_Shipments_ShipmentReferenceId",
+                        column: x => x.ShipmentReferenceId,
+                        principalTable: "Shipments",
+                        principalColumn: "ReferenceId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "OrganizationShipment",
+                columns: table => new
+                {
+                    OrganizationsId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    ShipmentsReferenceId = table.Column<string>(type: "nvarchar(450)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OrganizationShipment", x => new { x.OrganizationsId, x.ShipmentsReferenceId });
+                    table.ForeignKey(
+                        name: "FK_OrganizationShipment_Organizations_OrganizationsId",
+                        column: x => x.OrganizationsId,
+                        principalTable: "Organizations",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_OrganizationShipment_Shipments_ShipmentsReferenceId",
+                        column: x => x.ShipmentsReferenceId,
+                        principalTable: "Shipments",
+                        principalColumn: "ReferenceId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Nodes_ShipmentReferenceId",
+                table: "Nodes",
+                column: "ShipmentReferenceId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrganizationShipment_ShipmentsReferenceId",
+                table: "OrganizationShipment",
+                column: "ShipmentsReferenceId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Nodes");
+
+            migrationBuilder.DropTable(
+                name: "OrganizationShipment");
+
+            migrationBuilder.DropTable(
+                name: "Organizations");
+
+            migrationBuilder.DropTable(
+                name: "Shipments");
+        }
+    }
+}
